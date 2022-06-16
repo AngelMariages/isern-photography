bc4f5f340bde10ced955fa38602550e6331410e2
Àngel Mariages <angel.mariages@gmail.com>
2022-06-16 20:38:18 +0200

    test sizes

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -11,14 +11,22 @@ const BlurImage: NextPage<{ post: Post, isPriority: boolean }> = ({ post, isPrio
 	const [isLoading, setIsLoading] = useState(true);
 
 	return (
-		<div className='absolute inset-0 m-0 w-full bg-gray-200'>
+		<div className='absolute inset-0 m-0 w-full bg-gray-200 cursor-pointer'>
 			<Image
 				src={post.image}
 				alt={post.title}
+				sizes={
+					'(max-width: 640px) 100vw' +
+					',(max-width: 768px) 50vw' +
+					',(max-width: 1280px) 33vw' +
+					',(max-width: 1920px) 25vw'
+				}
 				layout='fill'
+				objectFit='fill'
+				loading='eager'
 				priority={isPriority}
 				className={cn(
-					'duration-700 ease-in-out group-hover:opacity-75',
+					'duration-700 ease-in-out hover:scale-110',
 					isLoading
 						? 'scale-110 blur-2xl grayscale'
 						: 'scale-100 blur-0 grayscale-0'
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 const Home: NextPage<Props> = ({ allPosts }) => {
   return (
     <div
-      className='columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2.5 grid-cols-4 p-0 m-0'
+      className='columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-0'
       style={{
         gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
       }}
@@ -18,7 +18,7 @@ const Home: NextPage<Props> = ({ allPosts }) => {
       {allPosts.map((post, id) => (
         <div key={id} className='relative w-full'>
           <div className='inline-flex' style={{
-            paddingTop: `${100 / (post.imageWidth / post.imageHeight)}%`
+            paddingTop: `calc(${100 / (post.imageWidth / post.imageHeight)}% - 10px)`
           }}>
             <BlurImage
               post={post}
@@ -32,20 +32,12 @@ const Home: NextPage<Props> = ({ allPosts }) => {
   );
 }
 
-const shuffle = (a: any[]) => {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-
 export async function getStaticProps() {
   const allPosts = getAllPosts();
 
   return {
     props: {
-      allPosts: shuffle([...allPosts.concat(allPosts).concat(allPosts)])
+      allPosts: [...allPosts.concat(allPosts).concat(allPosts)]
     }
   }
 }
