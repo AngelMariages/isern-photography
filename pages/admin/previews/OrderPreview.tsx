bd625a64c71e67c05f804fa53a0ab28ac188f2b8
Àngel Mariages <angel.mariages@gmail.com>
2022-09-15 01:34:12 +0200

    check for entry data

--- pages/admin/previews/OrderPreview.tsx
+++ pages/admin/previews/OrderPreview.tsx
@@ -23,15 +23,17 @@ type OrderPreviewProps = PreviewTemplateComponentProps & {
 const OrderPreview: React.FC<OrderPreviewProps> = ({ entry, allPosts }) => {
 	const [postsList, setPostsList] = useState<Post[]>([]);
 
-	const { postOrderList } = entry.get("data").toJS();
+	const { postOrderList } = entry?.get("data")?.toJS() || {};
 
 	useEffect(() => {
-		const postSlugs: string[] = postOrderList.map(({ postSlug }: { postSlug: string }) => postSlug);
+		if (postOrderList?.length) {
+			const postSlugs: string[] = postOrderList.map(({ postSlug }: { postSlug: string }) => postSlug);
 
-		setPostsList(
-			postSlugs.map((slug) => allPosts.find((post) => post.slug === slug)).filter(Boolean) as Post[]
-		)
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+			setPostsList(
+				postSlugs.map((slug) => allPosts.find((post) => post.slug === slug)).filter(Boolean) as Post[]
+			)
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [JSON.stringify(postOrderList)]);
 
 
