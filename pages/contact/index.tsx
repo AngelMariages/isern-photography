54169b26c1bc0339f99af796ec8e1112464954f1
Àngel Mariages <angel.mariages@gmail.com>
2022-10-25 00:00:48 +0200

    unify contact pages

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -8,6 +8,7 @@ import { object, string } from 'yup';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import ContactBody from '../../components/ContactBody';
 
 type FormValues = {
 	name: string;
@@ -56,86 +57,75 @@ const Contact = ({ sectionOrder }: InferGetStaticPropsType<typeof getStaticProps
 	};
 
 	return (
-		<div className='bg-gray-300'>
-			<Head>
-				<title>Jordi Isern Photography - Contact</title>
-			</Head>
-			<Header variant='light' sectionOrder={sectionOrder} />
-			<div className='h-screen text-gray-300'>
-				<div className='absolute h-screen w-full'>
-					<MainImage image={mainImage} />
-				</div>
-				<div className='pt-[8rem] relative z-[9] md:pl-20 pl-10'>
-					<div className='text-4xl text-medium md:py-20 py-10'>
-						Contact
-					</div>
-					<Formik
-						initialValues={{
-							name: '',
-							email: '',
-							message: '',
-							'g-recaptcha-response': '',
-						}}
-						validationSchema={object().shape({
-							name: string().required('Name is required'),
-							email: string().email('Invalid email').required('Email is required'),
-							message: string().min(10).max(500).required('Message is required'),
-							'g-recaptcha-response': string().required('Please verify you are not a robot'),
-						})}
-						onSubmit={submitForm}
-					>
-						{({
-							values,
-							touched,
-							errors,
-							handleChange,
-							handleBlur: originalHandleBlur,
-							handleSubmit,
-							setFieldValue,
-							setFieldError,
-						}) => {
-							const handleBlur = async (ev: any) => {
-								if (!executeRecaptcha) {
-									setFieldError('g-recaptcha-response', 'Please verify you are not a robot');
-									return;
-								}
-
-								const token = await executeRecaptcha('contact');
-
-								if (token) {
-									setFieldValue('g-recaptcha-response', token);
-								}
-
-								originalHandleBlur(ev);
-							};
-
-							return (
-								<form className='flex flex-col w-1/2 min-w-[20rem] max-w-[50%]' onSubmit={handleSubmit}>
-									<div className='flex flex-col mb-6'>
-										<label className='text-xl font-medium' htmlFor='name'>Name</label>
-										<input value={values.name} onBlur={handleBlur} onChange={handleChange} className='border border-gray-400 bg-transparent my-2 rounded-sm' type='text' name='name' id='name' />
-										{errors.name && touched.name && <div className='text-red-500'>{errors.name}</div>}
-									</div>
-									<div className='flex flex-col mb-6'>
-										<label className='text-xl font-medium' htmlFor='email'>Email</label>
-										<input value={values.email} onBlur={handleBlur} onChange={handleChange} className='border border-gray-400 bg-transparent my-2 rounded-sm' type='text' name='email' id='email' />
-										{errors.email && touched.email && <div className='text-red-500'>{errors.email}</div>}
-									</div>
-									<div className='flex flex-col mb-6'>
-										<label className='text-xl font-medium' htmlFor='message'>Message</label>
-										<textarea value={values.message} onBlur={handleBlur} onChange={handleChange} className='border border-gray-400 bg-transparent my-2 rounded-sm' name='message' id='message' cols={30} rows={10} />
-										{errors.message && touched.message && <div className='text-red-500'>{errors.message}</div>}
-									</div>
-									<button className='bg-gray-400 w-32 font-semibold text-black text-xl uppercase rounded-sm' type='submit'>
-										Send
-									</button>
-								</form>
-							);
-						}}
-					</Formik>
-				</div>
+		<ContactBody sectionOrder={sectionOrder}>
+			<div className='text-4xl text-medium md:py-18 py-10'>
+				Contact
 			</div>
-		</div>
+			<Formik
+				initialValues={{
+					name: '',
+					email: '',
+					message: '',
+					'g-recaptcha-response': '',
+				}}
+				validationSchema={object().shape({
+					name: string().required('Name is required'),
+					email: string().email('Invalid email').required('Email is required'),
+					message: string().min(10).max(500).required('Message is required'),
+					'g-recaptcha-response': string().required('Please verify you are not a robot'),
+				})}
+				onSubmit={submitForm}
+			>
+				{({
+					values,
+					touched,
+					errors,
+					handleChange,
+					handleBlur: originalHandleBlur,
+					handleSubmit,
+					setFieldValue,
+					setFieldError,
+				}) => {
+					const handleBlur = async (ev: any) => {
+						if (!executeRecaptcha) {
+							setFieldError('g-recaptcha-response', 'Please verify you are not a robot');
+							return;
+						}
+
+						const token = await executeRecaptcha('contact');
+
+						if (token) {
+							setFieldValue('g-recaptcha-response', token);
+						}
+
+						originalHandleBlur(ev);
+					};
+
+					return (
+						<form className='flex flex-col w-1/2 min-w-[20rem] max-w-[50%]' onSubmit={handleSubmit}>
+							<div className='flex flex-col mb-6'>
+								<label className='text-xl font-medium' htmlFor='name'>Name</label>
+								<input value={values.name} onBlur={handleBlur} onChange={handleChange} className='border border-gray-400 bg-transparent my-2 rounded-sm' type='text' name='name' id='name' />
+								{errors.name && touched.name && <div className='text-red-500'>{errors.name}</div>}
+							</div>
+							<div className='flex flex-col mb-6'>
+								<label className='text-xl font-medium' htmlFor='email'>Email</label>
+								<input value={values.email} onBlur={handleBlur} onChange={handleChange} className='border border-gray-400 bg-transparent my-2 rounded-sm' type='text' name='email' id='email' />
+								{errors.email && touched.email && <div className='text-red-500'>{errors.email}</div>}
+							</div>
+							<div className='flex flex-col mb-6'>
+								<label className='text-xl font-medium' htmlFor='message'>Message</label>
+								<textarea value={values.message} onBlur={handleBlur} onChange={handleChange} className='border border-gray-400 bg-transparent my-2 rounded-sm' name='message' id='message' cols={30} rows={8} />
+								{errors.message && touched.message && <div className='text-red-500'>{errors.message}</div>}
+							</div>
+							<button className='bg-gray-400 w-32 font-semibold text-black text-xl uppercase rounded-sm' type='submit'>
+								Send
+							</button>
+						</form>
+					);
+				}}
+			</Formik>
+		</ContactBody>
 	)
 };
 
--- pages/contact/thanks.tsx
+++ pages/contact/thanks.tsx
@@ -1,6 +1,5 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
-import Head from 'next/head';
-import Header from '../../components/Header';
+import ContactBody from '../../components/ContactBody';
 import { getSectionOrder } from '../../lib/api';
 
 export const getStaticProps: GetStaticProps<{ sectionOrder: string[] }> = async () => {
@@ -16,21 +15,13 @@ const Thanks = ({
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
 
 	return (
-		<div className='bg-gray-300'>
-			<Head>
-				<title>Jordi Isern Photography - Contact</title>
-			</Head>
-			<Header variant='light' sectionOrder={sectionOrder} />
-			<div className='pt-[8rem] h-screen bg-cover pl-20 text-gray-300' style={{
-				backgroundImage: 'url(/contact-bg.jpg)',
-			}}>
-				<div className='text-4xl text-medium pt-20'>
-					Thanks for contacting me!
-					<br />
-					I will get back to you as soon as possible.
-				</div>
+		<ContactBody sectionOrder={sectionOrder}>
+			<div className='text-4xl text-medium pt-20'>
+				Thanks for contacting me!
+				<br />
+				I will get back to you as soon as possible.
 			</div>
-		</div>
+		</ContactBody>
 	)
 };
 
