eee06bfd625e4438d1c39114329eef0f26187f60
Àngel Mariages <angel.mariages@gmail.com>
2023-01-07 21:19:17 +0100

    add parallax to MainImage

--- components/MainImage.tsx
+++ components/MainImage.tsx
@@ -1,23 +1,27 @@
-import Image from 'next/future/image';
-import { PostImage } from '../lib/api'
+import Image from "next/future/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { PostImage } from "../lib/api";
 
-const MainImage = ({ image }: {
-	image: PostImage
-}) => {
+const MainImage = ({ image }: { image: PostImage }) => {
+	const { scrollYProgress } = useScroll();
+	const y = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
+
 	return (
-		<Image
-			src={image.src}
-			alt='Main image'
-			priority
-			loading='eager'
-			className='object-cover w-full h-screen'
-			placeholder={image.blurDataURL ? 'blur' : undefined}
-			blurDataURL={image.blurDataURL}
-			width={image.width}
-			height={image.height}
-			quality={80}
-		/>
+		<motion.div style={{ y }}>
+			<Image
+				src={image.src}
+				alt="Main image"
+				priority
+				loading="eager"
+				className="object-cover w-full h-screen"
+				placeholder={image.blurDataURL ? "blur" : undefined}
+				blurDataURL={image.blurDataURL}
+				width={image.width}
+				height={image.height}
+				quality={80}
+			/>
+		</motion.div>
 	);
-}
+};
 
 export default MainImage;
